10d439c32bda6a7b6e60069c15c562ac937ec5a0
Daniel Weibel <danielmweibel@gmail.com>
2020-05-09 18:59:14 +0200

    Add default VPC example

---
+++ examples/default-vpc/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = var.region
+}
+
+module "cluster" {
+  source           = "weibeld/kubeadm/aws"
+  version          = "~> 0.0"
+  private_key_file = var.private_key_file
+  public_key_file  = var.public_key_file
+}
---
+++ examples/default-vpc/outputs.tf
@@ -0,0 +1,19 @@
+output "kubeconfig" {
+  value       = module.cluster.kubeconfig
+  description = "Absolute path of the kubeconfig file of the created cluster on the local machine."
+}
+
+output "cluster_name" {
+  value       = module.cluster.cluster_name
+  description = "Name of the created cluster. This name is used as the value of the \"kubeadm:cluster\" tag assigned to all created resources."
+}
+
+output "cluster_nodes" {
+  value       = module.cluster.cluster_nodes
+  description = "Name, public and private IP addresse, and subnet ID of all nodes of the created cluster."
+}
+
+output "vpc_id" {
+  value       = module.cluster.vpc_id
+  description = "ID of the AWS VPC in which the cluster has been created."
+}
