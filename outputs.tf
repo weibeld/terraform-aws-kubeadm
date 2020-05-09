87d22e067874df0b721dbaf871dc47aa4e6b077f
Daniel Weibel <danielmweibel@gmail.com>
2020-05-09 18:44:50 +0200

    Add vpc_id output to main module
    For the cases when the vpc_id variable is left unspecified in which case
    the cluster will be created in the default VPC. In this case, it may be
    necessary to retrieve the ID of the VPC in which the cluster is running.

--- outputs.tf
+++ outputs.tf
@@ -10,8 +10,14 @@ output "cluster_nodes" {
   value = [
     for i in concat([aws_instance.master], aws_instance.workers, ) : {
       name       = i.tags["kubeadm:node"]
+      subnet_id  = i.subnet_id
       private_ip = i.private_ip
       public_ip  = i.tags["kubeadm:node"] == "master" ? aws_eip.master.public_ip : i.public_ip
     }
   ]
+}
+
+output "vpc_id" {
+  value       = aws_security_group.egress.vpc_id
+  description = "The ID of the AWS VPC in which the cluster is running."
 }
