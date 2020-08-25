b131a4e6755aafc1b27afe90c03dbe0d88c43b7d
Peter ONeill <jaretep@gmail.com>
2020-08-25 18:48:35 +0200

    removed local-exec on null-data-source

--- main.tf
+++ main.tf
@@ -306,8 +306,5 @@ data "null_data_source" "kubeconfig_file" {
   depends_on = [
     null_resource.download_kubeconfig_file
   ]
-  provisioner "local-exec" {
-    command =  "until kubectl --kubeconfig ${local.kubeconfig_file} get ns; do sleep 2 && echo sleeping; done"
-  }
 }
 
