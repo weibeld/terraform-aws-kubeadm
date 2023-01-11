3254076535a34898cd030aee0f9395f79298a7a1
Daniel Weibel <danielmweibel@gmail.com>
2023-01-11 03:23:46 +0100

    Add master node RAM requirement to variable description
    For the 'master_instance_type' input variable.

--- variables.tf
+++ variables.tf
@@ -62,7 +62,7 @@ variable "pod_network_cidr_block" {
 
 variable "master_instance_type" {
   type        = string
-  description = "EC2 instance type for the master node (must have at least 2 CPUs)."
+  description = "EC2 instance type for the master node (must have at least 2 CPUs and 2 GB RAM)."
   default     = "t2.medium"
 }
 
